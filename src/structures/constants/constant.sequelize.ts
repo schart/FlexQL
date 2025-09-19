2f2811eb2f91c77a76c9c1ff365bcf0c5e4b461d
Heja <arslanheja202@gmail.com>
2025-09-19 12:10:23 +0300

    feat(sequelize) add template of sequelize but not implemented yet!

---
+++ src/structures/constants/constant.sequelize.ts
@@ -0,0 +1,13 @@
+import { Op } from "sequelize";
+
+export const SEQUZELIZE_OPERATORS: Record<any, any> = {
+  // Logic
+  and: Op.and,
+  or: Op.or,
+  // comparisons
+  ">": Op.gt,
+  "<": Op.lt,
+  "==": Op.eq,
+  ">=": Op.gte,
+  "<=": Op.lte,
+};
