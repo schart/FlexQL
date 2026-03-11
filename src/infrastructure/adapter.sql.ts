b6822ac6196388b8b8f67af6996d7923a152a2da
Xeja <innalcuzdan1@gmail.com>
2026-03-12 02:08:01 +0300

    feat: minimal fixes

--- src/infrastructure/adapter.sql.ts
+++ src/infrastructure/adapter.sql.ts
@@ -1,6 +1,6 @@
+import { BaseAstAdapter } from "./base.adapter";
 import { treeInterface } from "@/shared/interfaces/interface.tree";
 import { flexQLResultInterface } from "@/shared/interfaces/interface.adapter";
-import { BaseAstAdapter } from "./base.adapter";
 
 export class SQLAdapter extends BaseAstAdapter {
   protected readonly ast: treeInterface | any;
@@ -59,67 +59,5 @@ export class SQLAdapter extends BaseAstAdapter {
     };
   }
 
-  // execute(): flexQLResultInterface {
-  //   const values: string[] = [];
-  //   const ast: any = this.ast;
-  //   const rootLogic = ast.logic;
-  //   const conditions = ast.conditions;
-
-  //   if (Array.isArray(conditions) && conditions.length > 0) {
-  //     for (let i = 0; i < conditions.length; i++) {
-  //       const condition = conditions[i];
-
-  //       if (condition.conditions) {
-  //         this.whConditions.push("(");
-
-  //         const innerConditions = condition.conditions;
-  //         for (let j = 0; j < innerConditions.length; j++) {
-  //           const inner = innerConditions[j];
-
-  //           this.whConditions.push(
-  //             inner["column"],
-  //             inner["op"] === "==" ? "=" : inner["op"],
-  //             "?",
-  //           );
-
-  //           values.push(inner["value"]);
-
-  //           if (j < innerConditions.length - 1) {
-  //             this.whConditions.push(condition.logic);
-  //           }
-  //         }
-
-  //         this.whConditions.push(")");
-
-  //         if (i < conditions.length - 1) {
-  //           this.whConditions.push(rootLogic);
-  //         }
-  //       } else {
-  //         this.whConditions.push(
-  //           condition["column"],
-  //           condition["op"] === "==" ? "=" : condition["op"],
-  //           "?",
-  //         );
-  //         values.push(condition["value"]);
-
-  //         if (i < conditions.length - 1) {
-  //           this.whConditions.push(rootLogic);
-  //         }
-  //       }
-  //     }
-  //   } else {
-  //     // Process just one simple query
-  //     this.whConditions.push(ast.column, ast.op === "==" ? "=" : ast.op, "?");
-  //     values.push(ast.value);
-  //   }
-
-  //   return {
-  //     type: "sql",
-  //     payload: {
-  //       conditions:
-  //         this.whConditions.length === 1 ? "" : this.whConditions.join(" "),
-  //       values,
-  //     },
-  //   };
-  // }
+ 
 }
